5e9bc924f914f079889aa0856264280f6480ef36
prashantg <prashantg@valuefy.com>
2022-12-31 20:40:46 +0530

    feat: chapter 2 initialised

---
+++ chapter-02/index.js
@@ -0,0 +1,25 @@
+
+const header = React.createElement('h1', {
+  key: 'header1'
+  /**
+  Keys help React identify which items have changed, are add
+  ed, or are removed. Keys should be given to the elements inside the arr
+  ay to give the elements a stable identity */
+}, 'Text to be shown!');
+
+const header2 = React.createElement('h2', {
+  style: { color: 'red' },
+  key: 'header2'
+},
+  'Im a header2'
+);
+const container = React.createElement('div', {
+  id: 'container'
+},
+  [
+    header,
+    header2
+  ]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(container);
